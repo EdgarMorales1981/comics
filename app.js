87760b9892d41de2a9a090c7393be78ea4dda259
Edgar Morales <desarrolloaplicaciones1981@gmail.com>
2024-07-10 14:27:06 -0400

    Descripción de los cambios

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+document.querySelectorAll('.download-item h2').forEach(item => {
+    item.addEventListener('click', () => {
+        const dropdown = item.nextElementSibling;
+        dropdown.classList.toggle('show');
+    });
+});
+
+listado =[{
+    imagen:'./images/brunobrunner.jpg',
+    title: 'Bruno Brunner',
+},
+    {
+        imagen:'./images/corazondecristalmarketing.jpg',
+        title: 'Corazon de Cristal',
+    },
+    {
+        imagen:'./images/fabioliebremarketing.jpg',
+        title: 'Fabio Liebre',
+    },
+    {
+        imagen: './images/gingerboymarketing.jpg',
+        title: 'Ginger Boy'
+    },
+
+]
+
+
+
+
+const contenedor = document.querySelector('.fotoimagen');
+let index = 0;
+
+function cambiarImagen() {
+    contenedor.innerHTML = ''; // Limpiar el contenedor
+    const img = document.createElement('img');
+    const titulo = document.createElement('h2');
+    titulo.innerHTML = listado[index].title;
+    img.src = listado[index].imagen;
+    titulo.className = 'tituloFoto';
+    contenedor.appendChild(titulo)
+    contenedor.appendChild(img);
+    index = (index + 1) % listado.length; // Avanzar al siguiente índice, y volver al inicio si es necesario
+}
+
+setInterval(cambiarImagen, 5000); // Cambiar imagen cada segundo
+
+// Iniciar con la primera imagen
+cambiarImagen();
